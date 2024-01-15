e736a1b4b3bd4ccf3961df9f623d993bdcec0487
Devansh0309 <samdeva0309@gmail.com>
2024-01-15 15:50:19 +0530

    carousel completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import image1 from "./image1.jpg";
+import image2 from "./image2.webp";
 
 function App() {
+  const viewElement = useRef(null);
+  const [images, setImages] = useState([
+    { path: image1, ref: viewElement },
+    { path: image2, ref: null },
+    { path: image1, ref: null },
+    { path: image2, ref: null },
+  ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const doClick = () => {
+    viewElement.current.scrollIntoView({ behavior: "smooth" });
+    // console.log("inside doClick", images,currentIndex);
+  };
+
+  useEffect(() => {
+    // console.log("hello3", images, currentIndex);
+    doClick();
+  }, [images, currentIndex]);
+
+  const changeImagesRef = (x) => {
+    console.log("inside changeImages", currentIndex + x);
+    const imgs = images;
+    for (let i = 0; i < imgs.length; i++) {
+      if (i !== currentIndex + x) {
+        imgs[i].ref = null;
+      } else imgs[i].ref = viewElement;
+    }
+    setImages(imgs);
+    setCurrentIndex(currentIndex + x);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-carousel">
+        {images.map((item, index) => (
+          <img
+            src={item.path}
+            width="100%"
+            height="auto"
+            alt="photo"
+            ref={item.ref}
+            key={item.path + index}
+          />
+        ))}
+        {currentIndex > 0 ? (
+          <button
+            className="left-btn"
+            onClick={() => {
+              changeImagesRef(-1);
+            }}
+          >
+            {"<"}
+          </button>
+        ) : null}
+        {currentIndex < images.length - 1 ? (
+          <button
+            className="right-btn"
+            onClick={() => {
+              changeImagesRef(+1);
+            }}
+          >
+            {">"}
+          </button>
+        ) : null}
+      </div>
     </div>
   );
 }
